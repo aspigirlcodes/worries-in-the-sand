cce60ada37e08c0a9600bb211f0ff834d76b70ff
Sara <sara.peeters@gmail.com>
2018-06-12 08:58:00 +0200

    fixed the removal of all worrypaths by adding them to a compoundpath

--- js/sea.js
+++ js/sea.js
@@ -1,19 +1,19 @@
 var width, height, center;
 var points = 12;
-var path = new Path();
+var seaPath = new Path();
 var pathHeight;
 var speeds = [];
 var time = 0;
 var tide = 'low';
 var drag;
-var wetSand, worryPath, surprise, text, textContent;
+var wetSand, worryPath, worryCompound, surprise, text, textContent;
 var surprises = ['img/seashell.png',
                  'img/starfish.png',
                  'img/stone.png',
                  'img/bottle.png',
                  'img/alga.png'];
 tool.minDistance = 10;
-path.fillColor = '#133468';
+seaPath.fillColor = '#133468';
 initializePath();
 
 function randomInt(min, max){
@@ -26,20 +26,24 @@ function initializePath() {
 	pathCenter = height * 15/16
   pathHeight = height * 7/8
   textContent = "";
-	path.segments = [];
-	path.add(view.bounds.bottomLeft);
+	seaPath.segments = [];
+	seaPath.add(view.bounds.bottomLeft);
+  worryCompound = new CompoundPath().insertBelow(seaPath);
+  worryCompound.strokeColor = '#c1b460';
+  worryCompound.strokeWidth = 10;
+  worryCompound.strokeCap = 'round';
   speeds.push(0)
 	for (var i = 0; i < points-1; i++) {
 		var point = new Point(width / (points - 2) * i, pathCenter);
-		path.add(point);
+		seaPath.add(point);
     speeds.push(randomInt(0,10) / 2.0)
 	}
-	path.add(view.bounds.bottomRight);
+	seaPath.add(view.bounds.bottomRight);
   speeds.push(0)
 }
 
 function ebbSurprise(){
-  wetSand = path.clone().insertBelow(path);
+  wetSand = seaPath.clone().insertBelow(seaPath);
   wetSand.fillColor = '#c1b460';
   var randomPosition = new Point(randomInt(50, width - 50),
                                  randomInt(150, height - 150));
@@ -47,11 +51,10 @@ function ebbSurprise(){
                          position: randomPosition}).insertAbove(wetSand);
   if (typeof worryPath !== 'undefined'){
     console.log("removing worrypath")
-    worryPath.strokeColor = "blue";
-    worryPath = null;
+    worryCompound.remove();
   }
   if(typeof text !== 'undefined'){
-    text.remove()
+    text.remove();
   }
   textContent = "";
 }
@@ -59,16 +62,17 @@ function ebbSurprise(){
 function cleanBeach(){
   wetSand.remove();
   surprise.remove();
-  initializePath()
+  initializePath();
 }
 
 function onMouseDown(event) {
   drag = false;
-  if (tide == 'low'){
-    worryPath = new Path().insertBelow(path);
-	  worryPath.strokeColor = '#c1b460';
+  if (tide == 'low' && event.point.y <= pathHeight){
+    worryPath = new Path().insertBelow(seaPath);
+    worryPath.strokeColor = '#c1b460';
     worryPath.strokeWidth = 10;
     worryPath.strokeCap = 'round';
+
   }
 }
 
@@ -85,9 +89,13 @@ function onMouseUp(event){
       hideInstructions();
       tide = 'flood';
     } else if (tide == 'ebb'){
-      cleanBeach()
-      showInstructions()
-      tide = 'low'
+      cleanBeach();
+      showInstructions();
+      tide = 'low';
+    }
+  } else if (tide == 'low') {
+    if(typeof worryPath !== 'undefined'){
+      worryCompound.addChild(worryPath);
     }
   }
 }
@@ -102,7 +110,6 @@ function onKeyDown(event) {
 	text.content = textContent ;
 }
 
-
 function onFrame(event) {
   if (tide == 'flood'){
     pathCenter--;
@@ -116,13 +123,13 @@ function onFrame(event) {
 	for (var i = 1; i < points; i++) {
 		var sinHeight = Math.sin(time* speeds[i]/100) * (pathHeight - pathCenter);
 		var yPos = sinHeight + pathCenter
-		path.segments[i].point.y = yPos;
+		seaPath.segments[i].point.y = yPos;
     if(tide=='flood' && yPos <= 0){
       ebbSurprise()
       tide = 'ebb'
     }
 	}
-	path.smooth({ type: 'continuous' });
+	seaPath.smooth({ type: 'continuous' });
 }
 
 this.typeInit = function(){
@@ -133,7 +140,7 @@ this.typeInit = function(){
     	justification: 'center',
     	fontSize: 30,
       strokeColor: '#c1b460',
-    }).insertBelow(path);
+    }).insertBelow(seaPath);
   }
 }
 
